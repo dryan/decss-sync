9e06ba74490404cb5d74688fe93b7a8bf2539e91
Daniel Ryan <dryan@dryan.com>
2016-02-14 13:32:34 -0500

    improving wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
-from sync import main
+import tornado.wsgi
+import wsgiref.simple_server
+from sync import Application
 
 if __name__ == '__main__':
-    main()
+    application = Application()
+    wsgi_app = tornado.wsgi.WSGIAdapter(application)
+    server = wsgiref.simple_server.make_server('', 8888, wsgi_app)
+    server.serve_forever()
